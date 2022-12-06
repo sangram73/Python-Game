75810fc6a55e01a008fcd6b361f4526c031988a2
Sangram <panda.sangram20@gmail.com>
2022-12-07 02:54:45 +0530

    add how score display in screen

--- scorebord.py
+++ scorebord.py
@@ -1,6 +1,25 @@
 from turtle import Turtle
-
+FONT=("Courien",20,"bold")
 
 class Score(Turtle):
-    def __init__(self) -> None:
+    def __init__(self,totalife) -> None:
         super().__init__()
+        self.score=totalife
+        self.penup()
+        self.goto(-250,225)
+        self.fillcolor("black")
+        self.pendown()
+        self.forward(500)
+        self.left(90)
+        self.forward(23)
+        self.left(90)
+        self.forward(500)
+        self.left(90)
+        self.forward(23)
+        self.penup()
+        self.setheading(0)
+        self.goto(0,233)
+        self.color("Crimson")
+        self.write(f"YOU HAVE {self.score} LIFE",align="center", font=FONT)
+        self.hideturtle()
+        
