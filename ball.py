e84ec47b350aab010303f0e0d44bd7ee9457df34
Sangram <panda.sangram20@gmail.com>
2022-12-07 02:05:12 +0530

    adding reset ball position wen ball fall

--- ball.py
+++ ball.py
@@ -11,6 +11,17 @@ class Ball(Turtle):
         self.y_pos = 10
 
     def movo(self):
-        new_x=self.xcor()-self.x_pos
-        new_y=self.ycor()-self.y_pos
+        new_x = self.xcor()-self.x_pos
+        new_y = self.ycor()-self.y_pos
+        self.goto(new_x, new_y)
+
+    def bounce_x(self):
+        self.x_pos *=-1
+
+    def bounce_y(self):
+        self.y_pos *=-1
         
+        
+    def reset(self):
+        self.goto(0,0)
+        self.bounce_y()
