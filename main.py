9c4c600fc83e8689af5fb4f4063e120f128a1ec1
Sangram <panda.sangram20@gmail.com>
2022-12-07 00:52:46 +0530

    creating box in randomize multi color

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from turtle import Turtle, Screen
 import time
+
+
 t = Turtle()
 screen = Screen()
-screen.setup(500,500)
+screen.setup(500, 500)
 screen.title("Bubble shooter game")
 # it will turn off the animation
 screen.tracer(0)
+screen.exitonclick()
