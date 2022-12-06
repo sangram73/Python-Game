152a45a5ab5770408db98811320c84415546ff1f
Sangram <panda.sangram20@gmail.com>
2022-12-06 23:14:03 +0530

    intilize the turtle scree for game

---
+++ main.py
@@ -0,0 +1,8 @@
+from turtle import Turtle, Screen
+import time
+t = Turtle()
+screen = Screen()
+screen.setup(500,500)
+screen.title("Bubble shooter game")
+# it will turn off the animation
+screen.tracer(0)
