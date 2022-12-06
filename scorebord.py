4dbddcb2f82d78aa839c51c9220612f881084d20
Sangram <panda.sangram20@gmail.com>
2022-12-07 03:11:54 +0530

    add when ball miss the paddle life loss

--- scorebord.py
+++ scorebord.py
@@ -23,3 +23,24 @@ class Score(Turtle):
         self.write(f"YOU HAVE {self.score} LIFE",align="center", font=FONT)
         self.hideturtle()
         
+    def life_decrise(self):
+        self.penup()
+        self.score=self.score-1
+        self.setheading(0)
+        self.goto(0,223)
+        self.color("crimson")
+        self.clear()
+        self.write(f"YOU HAVE {self.score} LIFE",align="center", font=FONT)
+        self.pencolor("black")
+        self.fillcolor("black")
+        self.goto(-250,225)
+        self.pendown()
+        self.forward(500)
+        self.left(90)
+        self.forward(23)
+        self.left(90)
+        self.forward(500)
+        self.left(90)
+        self.forward(23)
+                
+        
