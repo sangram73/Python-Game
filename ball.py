239e43d33b0bc7e7fed8ef3f9da5ec886e6049d5
Sangram <panda.sangram20@gmail.com>
2022-12-07 01:58:05 +0530

    add move function

--- ball.py
+++ ball.py
@@ -1,4 +1,4 @@
-    from turtle import Turtle
+from turtle import Turtle
 
 
 class Ball(Turtle):
@@ -7,5 +7,10 @@ class Ball(Turtle):
         self.penup()
         self.shape("circle")
         self.color("black")
-        
+        self.x_pos = 10
+        self.y_pos = 10
+
+    def movo(self):
+        new_x=self.xcor()-self.x_pos
+        new_y=self.ycor()-self.y_pos
         
